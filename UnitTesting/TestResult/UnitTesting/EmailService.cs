458c0b7d574e838aa873a2404b4bb75e812ac4d0
saili-100 <102532874+saili-100@users.noreply.github.com>
2025-09-18 12:45:54 +0530

    Add files via upload

---
+++ UnitTesting/TestResult/UnitTesting/EmailService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting
+{
+    public interface INotificationService
+    {
+        bool SendNotification(string deviceInfo, string message);
+    }
+
+    public class EmailService : INotificationService
+    {
+        public bool SendNotification(string deviceInfo, string message)
+        {
+            Console.WriteLine($"Email sent to {deviceInfo} with message:{message}");
+            return true;
+        }
+    }
+
+    public class SmsNotification : INotificationService
+    {
+        public bool SendNotification(string deviceInfo, string message)
+        {
+            Console.WriteLine($"Sms sent to {deviceInfo} with message:{message}");
+            return true;
+        }
+    }
+   
+
+    public class NotificationService
+    {
+        private readonly INotificationService _notificationService;
+        public NotificationService(INotificationService service)
+        {
+            this._notificationService = service;
+        }
+
+        public void NotifyUser(string deviceInfo, string message) {
+        //Simulate sending a notification
+        _notificationService.SendNotification(deviceInfo, message);
+        }
+    }
+}
+
+
+
+    
---
+++ UnitTesting/TestResult/UnitTesting/MathClass.cs
@@ -0,0 +1,12 @@
+﻿namespace UnitTesting
+{
+    public class MathClass
+    {
+        public double Addfunc(double num1, double num2)=> num1+num2;
+        public double Subfunc (double num1, double num2)=>num1-num2;
+
+        public double Mulfunc(double num1, double num2)=>num1*num2;
+        public double Divfunc(double num1, double num2)=>num1/num2;
+
+    }
+}
