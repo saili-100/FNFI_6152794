0e83a06633ab94a441cba2aafe9ea6e868904cee
saili-100 <102532874+saili-100@users.noreply.github.com>
2025-09-18 12:18:54 +0530

    Add files via upload

---
+++ DotnetCoreApps/BlazorApp.shared/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorApp.shared
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column("ProductName")]
+        public string Name { get; set; }
+        [Column("ProductPrice")]
+        public decimal Price { get; set; }
+        [Column("ProductQuantity")]
+        public int Quantity { get; set; }
+
+    }
+}
+
