85525f15974156005c644d0a7cb75e5c1dee1bfa
saili-100 <102532874+saili-100@users.noreply.github.com>
2025-09-18 12:46:49 +0530

    Add files via upload

---
+++ UnitTesting/UnitTestingComponent/MathTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace UnitTestingComponent
+{
+    //All test methods should be public,return Void and have the[TestMethod] attribute and no parameters.
+    [TestClass]
+    public sealed class MathTest
+    {
+        [TestMethod] //Attribute to indicate that this method is a unit test
+        public void AddPositiveNumbersTest()
+        {
+            //Arrange 
+            var component = new UnitTesting.MathClass();
+            var num1 = 5;
+            var num2 = 10;
+            var expected = 15;
+            //var expected = num1<0 && num2<0 ? 1 :0;
+
+            //Act 
+            var result = component.Addfunc(num1, num2);
+
+            //  Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void AddNegativeNumbersTest()
+        {
+            var component = new UnitTesting.MathClass();
+            var num1 = -5;
+            var num2 = -10;
+            var expected = -15;
+            //Act
+            var result = component.Addfunc(num1, num2);
+            Assert.AreEqual(expected, result);
+
+            //test for negative numbers 
+            //Test for zero
+        }
+
+        [TestMethod]
+        public void AddZeroNumbersTest()
+        {
+            var component = new UnitTesting.MathClass();
+            var num1 = 5;
+            var num2 = 0;
+            var expected = 5;
+
+            //Act 
+            var result = component.Subfunc(num1, num2);
+
+            Assert.AreEqual(expected, result);
+
+        }
+    }
+}
---
+++ UnitTesting/UnitTestingComponent/NotificationTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+
+namespace UnitTestingComponent
+{
+    [TestClass]
+    public class NotificationTests
+    {
+        [TestMethod]
+        public void ShallSendNotificationEmail()
+        {
+            var emailService = new UnitTesting.EmailService();
+            var notificationComponent = new UnitTesting.NotificationService(emailService);
+            var deviceInfo = "saili@gmail.com";
+            var message = "This is a test notification.";
+
+            notificationComponent.NotifyUser(deviceInfo, message);
+
+        }
+        [TestMethod]
+        public void ShallSendNotificationSms()
+        {
+            var smsService = new UnitTesting.SmsNotification();
+            var notificationComponent = new UnitTesting.NotificationService(smsService);
+            var deviceInfo = "Amulya.bn@fnf.com";
+            var message = "This is a test notification.";
+
+            notificationComponent.NotifyUser(deviceInfo, message);
+
+        }
+
+
+        [TestMethod]
+        public void ShallSendNotificationMock()
+        {
+            //aRRANGE 
+            var mockNotificationService = new Mock<UnitTesting.INotificationService>();
+
+            //Create a mock object of the InotificationService interface
+            mockNotificationService.Setup(service => service.SendNotification(
+                It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+
+            //Set up the mock to return true when send notification is called with string parameters
+            var notificationComponent = new UnitTesting.NotificationService(mockNotificationService.Object); //Get the mock object using the object property
+
+            var deviceInfo = "saili@gmail.com";
+            var message = "This is a test notification.";
+
+            //Act 
+            notificationComponent.NotifyUser(deviceInfo, message);
+
+            //Assert
+
+            mockNotificationService.Verify(service => service.SendNotification(deviceInfo,message), Times.Once());
+        }
+    }
+}
+
+
+
+
